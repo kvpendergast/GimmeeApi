23bacc41f2ddf7b929e2bab301acd40dbd95564c
BiiGRiiG <kvpendergast@gmail.com>
2015-06-07 12:43:32 -0400

    Added products table and controller and updated views to display all users and all products

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607154254) do
+ActiveRecord::Schema.define(version: 20150607162612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "productName"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "firstName"
