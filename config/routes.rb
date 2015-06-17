cbb5d8b80c050d08df70cc5d4a86d65d34a455d2
BiiGRiiG <kvpendergast@gmail.com>
2015-06-17 11:27:03 -0400

    Added show a user's activities route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, :products, :suppliers, :activities
 
+  get '/users/:id/activities', to: 'activities#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
