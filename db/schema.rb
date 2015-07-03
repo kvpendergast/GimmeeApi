87f091ab91e63857d535a4fed8ae8ba4299dfcb1
BiiGRiiG <kvpendergast@gmail.com>
2015-07-03 11:14:25 -0400

    Added url column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703141709) do
+ActiveRecord::Schema.define(version: 20150703150937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150703141709) do
     t.datetime "updated_at",                           null: false
     t.integer  "supplier_id"
     t.decimal  "price",       precision: 10, scale: 2
+    t.text     "url"
   end
 
   create_table "suppliers", force: :cascade do |t|
