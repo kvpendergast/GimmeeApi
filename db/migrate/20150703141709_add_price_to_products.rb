26a996e8fbc554fea304443d48053dbf961b6774
BiiGRiiG <kvpendergast@gmail.com>
2015-07-03 10:32:19 -0400

    Added price column to products table

---
+++ db/migrate/20150703141709_add_price_to_products.rb
@@ -0,0 +1,5 @@
+class AddPriceToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :price, :decimal, :precision => 10, :scale => 2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617201316) do
+ActiveRecord::Schema.define(version: 20150703141709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,10 @@ ActiveRecord::Schema.define(version: 20150617201316) do
   create_table "products", force: :cascade do |t|
     t.string   "productName"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.integer  "supplier_id"
+    t.decimal  "price",       precision: 10, scale: 2
   end
 
   create_table "suppliers", force: :cascade do |t|
