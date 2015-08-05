b773d4693c857dd0ba443ff24a7f6374fdf69117
BiiGRiiG <kvpendergast@gmail.com>
2015-08-04 21:32:19 -0400

    Changed time stamp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
 def amazonSignature(itemIds, associateTag, operation, responseGroup, service)
 	@accesskey = "AKIAIHMGKYWGXDTROKMA"
-	@timestamp = "2015-08-02T19%3A37%3A00Z"
+	@timestamp = "2015-08-10T19%3A37%3A00Z"
 	@securitykey = "R0pkbBP5YgtpR7Qqt+UrLy0HzxB/8PbnKIbyeDSX"
 	@data = "GET\nwebservices.amazon.com\n/onca/xml\nAWSAccessKeyId=#{@accesskey}&AssociateTag=#{associateTag}&ItemId=#{itemIds[0]}%2C#{itemIds[1]}%2C#{itemIds[2]}%2C#{itemIds[3]}%2C#{itemIds[4]}%2C#{itemIds[5]}%2C#{itemIds[6]}%2C#{itemIds[7]}%2C#{itemIds[8]}%2C#{itemIds[9]}&Operation=#{operation}&ResponseGroup=#{responseGroup}&Service=#{service}&Timestamp=#{@timestamp}"
 	@hash = OpenSSL::HMAC.digest('sha256',@securitykey,@data)
