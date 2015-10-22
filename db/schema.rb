596f53bf1c6b4aa4e45c8ddd0d6b8fbb13614288
kvpendergast <kvpendergast@gmail.com>
2015-10-21 23:57:14 -0400

    Updated productqueues with tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014022753) do
+ActiveRecord::Schema.define(version: 20151022032722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151014022753) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "productids", default: [],              array: true
+    t.string   "tag"
   end
 
   create_table "products", force: :cascade do |t|
@@ -71,6 +72,7 @@ ActiveRecord::Schema.define(version: 20151014022753) do
     t.text     "imageurl"
     t.string   "externalId"
     t.string   "detailPageUrl"
+    t.string   "tag"
   end
 
   create_table "suppliers", force: :cascade do |t|
