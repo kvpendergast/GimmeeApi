af6ba8ae775ffc3d2d7c11f67ac48fd0743ffd23
kvpendergast <kvpendergast@gmail.com>
2016-01-08 23:15:16 -0500

    Updated json response for user post to return whole user object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,15 +36,15 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		response_hash = Hash.new
 		if @user.update(user_params)
-			response_hash["status"] = 200
 			response_hash["message"] = "User Successfully Created"
 			response_hash["id"] = @user.id
-			render json: response_hash, status: 200
+			render json: response_hash, status: 201
 		else 
-			response_hash["status"] = 403
-			response_hash["message"] = "User already exists"
-			response_hash["id"] = User.find_by_facebook_id(user_params[:facebook_id]).id
-			render json: response_hash, status: 403
+			#response_hash["status"] = 403
+			#response_hash["message"] = "User already exists"
+			#response_hash["user"] = User.find_by_facebook_id(user_params[:facebook_id]).to_json
+			@user = User.find_by_facebook_id(user_params[:facebook_id]).to_json
+			render json: @user
 		end
 	end
 
