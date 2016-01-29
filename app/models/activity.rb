b3dad3c8de077746535c62e815704579b6f9bce8
kvpendergast <kvpendergast@gmail.com>
2016-01-28 20:20:00 -0500

    Fixed activity creation bug

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,6 +5,7 @@ class Activity < ActiveRecord::Base
   #relations
   belongs_to :user
   belongs_to :product
+  belongs_to :channel
   has_many :shared_activities
   belongs_to :shared_with_friend_activity, :class_name => 'shared_activities'
 end
