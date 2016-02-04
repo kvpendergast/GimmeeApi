950ef15dd468e48bcf5dd8b3cb8476baee5c7c1f
kvpendergast <kvpendergast@gmail.com>
2016-02-04 07:20:06 -0500

    Removed a migration

--- db/migrate/20160129023407_add_default_to_parent_channels.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultToParentChannels < ActiveRecord::Migration
-  def change
-  	remove_column :parent_channels, :defualt
-  	add_column :parent_channels, :default, :boolean
-  end
-end
--- db/migrate/20160129024414_fix_default_on_parent_channels.rb
+++ db/migrate/20160129024414_fix_default_on_parent_channels.rb
@@ -1,6 +1,5 @@
 class FixDefaultOnParentChannels < ActiveRecord::Migration
   def change
-  	remove_column :parent_channels, :defualt
   	add_column :parent_channels, :default, :boolean
   end
 end
